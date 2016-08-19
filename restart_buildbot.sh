fd66f0179a380820080895cced595ba83ed769fe
Jean Wu <jean.wu@mail.utoronto.ca>
2016-08-19 15:15:02 -0400

    Fix restart_buildbot.sh and delete irc bot in master.cfg

--- restart_buildbot.sh
+++ restart_buildbot.sh
@@ -1,11 +1,18 @@
+echo "=============================="
+echo "Restarting Buildbot on $(date)"
+
+cd /home/vtrbot/vtr-buildbot
+
+echo "Looking for slaves in $(pwd)"
 slaves=$(ls | grep "slave")
 
+echo "Found slaves: $slaves"
+
+echo "Starting Build Master"
 buildbot restart buildmaster
 
 for slave in $slaves; do
-	echo $slave
+	echo "Starting Slave $slave"
 	buildslave restart $slave
 done
-
-#start_server.sh
-
+echo "=============================="
